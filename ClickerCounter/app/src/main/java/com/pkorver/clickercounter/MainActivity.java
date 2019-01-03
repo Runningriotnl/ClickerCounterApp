6f4eaba9ae826d4eb54917e0fc4e73232acb920a
pkorver <pkorver@tmns.com>
2019-01-03 18:02:57 +0100

    Restarted in Java and redid layout

---
+++ ClickerCounter/app/src/main/java/com/pkorver/clickercounter/MainActivity.java
@@ -0,0 +1,19 @@
+package com.pkorver.clickercounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int id = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+
+
+}
