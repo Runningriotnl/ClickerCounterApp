7ca0f7671b0574d8d891eaab9cdb53b7d49fc601
pkorver <pkorver@tmns.com>
2019-01-04 11:17:04 +0100

    Count increase and decrease function

--- ClickerCounter/app/src/main/java/com/pkorver/clickercounter/MainActivity.java
+++ ClickerCounter/app/src/main/java/com/pkorver/clickercounter/MainActivity.java
@@ -2,18 +2,33 @@ package com.pkorver.clickercounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int id = 0;
+    TextView counterValue;
+    int counter = 0; // Counter starts at 0
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        counterValue = (TextView) findViewById(R.id.value);
+
     }
 
+    public void countIncrease(View view) {
+        // On each click the counter will increase 1 point
+        counter++;
+        counterValue.setText(Integer.toString(counter));
+    }
+
+    public void countDecrease(View v) {
+        // On each click remove 1 point from counter
+        counter--;
+        counterValue.setText(Integer.toString(counter));
+    }
 
 }
